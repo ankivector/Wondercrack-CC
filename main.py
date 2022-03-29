7955b755e8ef6185ec86165208e061286d75ac1b
OshadhaVimukthi <87273124+OshadhaVimukthiM@users.noreply.github.com>
2022-03-29 08:05:27 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,31 @@
+#pylint:disable=C0114
+import logging
+import os
+from pyrogram import Client
+from pyrogram.errors import RPCError
+from pyrogram.errors import BadRequest
+# import asyncio
+# from pyrogram.errors import FloodWait
+# from pyrogram.handlers import MessageHandler
+# os.environ['TZ'] = 'Asia/Kolkata'
+
+
+
+logging.basicConfig(level=logging.INFO)
+
+
+
+bot = Client(
+    'bot',
+    api_id= 6690144, #get it from https://my.telegram.org/auth
+    api_hash="be8e6aed33f43925819ba03044876a17", #get it from https://my.telegram.org/auth
+    bot_token="5193680142:AAEcLLljAIjzjdriPW_UcRd7j4C77ArV7tA", #get it from @Botfather
+    plugins=dict(root="plugins"),
+    parse_mode="html")
+
+
+try:
+    bot.run()
+except Exception as e:
+    print(e)
+        
